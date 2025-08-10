c482b5ae8f584236228a9ea0eb9a5cb8984cd8ad
ahmedtalaat670 <atalaat382003@gmail.com>
2025-08-11 01:10:59 +0200

    blabla

--- src/api/axiosInstace.js
+++ src/api/axiosInstace.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://learning-managing-system-backend-qd.vercel.app/",
 });
 
 axiosInstance.interceptors.request.use(
