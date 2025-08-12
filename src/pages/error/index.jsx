b2640096a1ffd583ceac4b7980e9b5ae01cbfa6c
ahmedtalaat670 <atalaat382003@gmail.com>
2025-08-13 00:55:44 +0200

    blabla

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,10 +1,26 @@
+import { ArrowBigLeft, HomeIcon } from "lucide-react";
 import React from "react";
 import { useRouteError } from "react-router-dom";
 
 const Error = () => {
   const error = useRouteError();
   console.log(error);
-  return <div>there is an error : {error}</div>;
+  return (
+    <div>
+      <h1>uh oh! you got an error:;</h1>
+      <h3>{error.message || error.statusText}</h3>
+      <div className="flex gap-5">
+        <div className="capitalize">
+          <ArrowBigLeft />
+          go back{" "}
+        </div>
+        <div className="capitalize">
+          <HomeIcon />
+          go home
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
