a4989542bd529efe6d8dd57d60aaa4634a6a34b0
ahmedtalaat670 <atalaat382003@gmail.com>
2025-08-13 14:12:41 +0200

    blabla

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,13 @@ import App from "./App.jsx";
 import AuthContextProvider from "./context/auth-context";
 import InstructorProvider from "./context/instructor-context";
 import StudentContextProvider from "./context/student-context";
-import StandardErrorBoundary from "./pages/error-boundary";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthContextProvider>
       <InstructorProvider>
         <StudentContextProvider>
-          <StandardErrorBoundary>
-            <App />
-          </StandardErrorBoundary>
+          <App />
         </StudentContextProvider>
       </InstructorProvider>
     </AuthContextProvider>
