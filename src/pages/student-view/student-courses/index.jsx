752dc2b3753a0c93ec57b85fa8ca0700c6888a3a
ahmedtalaat670 <atalaat382003@gmail.com>
2025-09-12 20:16:17 +0200

    bla

--- src/pages/student-view/student-courses/index.jsx
+++ src/pages/student-view/student-courses/index.jsx
@@ -39,41 +39,45 @@ const StudentCourses = () => {
       <div className="p-4">
         <h1 className="text-3xl font-bold mb-8">My Courses</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
-          {!loadingState && boughtCourses && boughtCourses.length > 0 ? (
-            boughtCourses.map((course) => (
-              <Card
-                key={course.courseId}
-                className={`flex flex-col transition-all duration-700 ${
-                  animated && `-translate-y-5 opacity-0`
-                }`}
-              >
-                <CardContent className="p-4 flex-grow">
-                  <img
-                    src={course?.courseImage}
-                    alt={course?.title}
-                    className="h-52 w-full object-cover rounded-md mb-4"
-                  />
-                  <h3 className="font-bold mb-1 capitalize">{course?.title}</h3>
-                  <p className="text-sm text-gray-700 mb-2">
-                    {course?.instructorName}
-                  </p>
-                </CardContent>
-                <CardFooter>
-                  <Button
-                    className="flex-1 cursor-pointer"
-                    onClick={() =>
-                      navigate(`/courses/course-progress/${course.courseId}`)
-                    }
-                  >
-                    <Watch className="mr-2 h-4 w-4" />
-                    Start Watching
-                  </Button>
-                </CardFooter>
-              </Card>
-            ))
-          ) : (
-            <h1 className="text-3xl font-bold">No Courses found</h1>
-          )}
+          {!loadingState &&
+            boughtCourses &&
+            (boughtCourses.length > 0 ? (
+              boughtCourses.map((course) => (
+                <Card
+                  key={course.courseId}
+                  className={`flex flex-col transition-all duration-700 ${
+                    animated && `-translate-y-5 opacity-0`
+                  }`}
+                >
+                  <CardContent className="p-4 flex-grow">
+                    <img
+                      src={course?.courseImage}
+                      alt={course?.title}
+                      className="h-52 w-full object-cover rounded-md mb-4"
+                    />
+                    <h3 className="font-bold mb-1 capitalize">
+                      {course?.title}
+                    </h3>
+                    <p className="text-sm text-gray-700 mb-2">
+                      {course?.instructorName}
+                    </p>
+                  </CardContent>
+                  <CardFooter>
+                    <Button
+                      className="flex-1 cursor-pointer"
+                      onClick={() =>
+                        navigate(`/courses/course-progress/${course.courseId}`)
+                      }
+                    >
+                      <Watch className="mr-2 h-4 w-4" />
+                      Start Watching
+                    </Button>
+                  </CardFooter>
+                </Card>
+              ))
+            ) : (
+              <h1 className="text-3xl font-bold">No Courses found</h1>
+            ))}
         </div>
       </div>
     </div>
